3b2a90e13bd0372ebba21dd85d23583284c61d64
Nytheneals <nythes@gmail.com>
2018-01-31 23:41:49 -0500

    made code improvements to the containers

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,14 +1,14 @@
-export default function() {
+export default function () {
   return [
-    { title: "Black Panther" },
-    { title: "Fifty shades freed" },
-    { title: "Den of Thieves" },
-    { title: "12 Strong" },
-    { title: "Proud Mary" },
-    { title: "Maze Runner" },
-    { title: "Insidious: The Last Key" },
-    { title: "Phantom Thread" },
-    { title: "The Post" },
-    { title: "Downsizing" }
+    { title: '⭐️⭐️⭐️⭐️  Black Panther' },
+    { title: '⭐️⭐️ Fifty shades freed' },
+    { title: '⭐️⭐️⭐️⭐️⭐️ Den of Thieves' },
+    { title: '⭐️⭐️ 12 Strong' },
+    { title: '⭐️⭐️⭐️ Proud Mary' },
+    { title: '⭐️⭐️⭐️⭐️  Maze Runner' },
+    { title: '⭐️⭐️ Insidious: The Last Key' },
+    { title: '⭐️⭐️ Phantom Thread' },
+    { title: '⭐️ The Post' },
+    { title: '⭐️⭐️⭐️⭐️ Downsizing' },
   ];
 }

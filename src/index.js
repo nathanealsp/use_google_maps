871b3a1f01d1beba9ca0200aed8d588ca594773e
Nytheneals <nythes@gmail.com>
2019-06-04 23:51:30 -0400

    updated to create react app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import App from "./components/app";
-import reducers from "./reducers";
+ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(
-  <Provider store={createStore(reducers)}>
-    <App />
-  </Provider>,
-  document.querySelector(".container")
-);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
